e2fe12ab0ff17f3705b6411ed423c4c866234e84
Ali <aliabbas808000@gmail.com>
2025-01-14 07:13:46 +0100

    fix formatting issues with prettier

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -9,7 +9,7 @@ export const createQuestionElement = (question) => {
   const element = document.createElement('div');
 
   // I use String.raw just to get fancy colors for the HTML in VS Code.
-  
+
   element.innerHTML = String.raw`
   <h1>${question}</h1>
 
