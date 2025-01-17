741ef1d836ec1843bf10516965ad6bf855e29ad4
Volodymyr-coder <volodymyr.coder2024@gmail.com>
2025-01-17 12:12:24 +0100

    add logics and css  for wrong and correct answer

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -23,14 +23,34 @@ export const initQuestionPage = () => {
     const answerElement = createAnswerElement(key, answerText);
     answersListElement.appendChild(answerElement);
 
-    // found element with tag input and listener. Look checked radioButton or not. if yes value this btn is  currentQuestion.selected
-    answerElement
-      .querySelector('input')
-      .addEventListener('change', function () {
-        if (this.checked) {
-          currentQuestion.selected = this.id;
+    const listItems = document.querySelectorAll('li');
+
+    // found element with input and listener?Look checked radioButton or not. if yes value this btn is  currentQuestion.selected
+    const input = answerElement.querySelector('input');
+    input.addEventListener('change', function () {
+      if (this.checked) {
+        currentQuestion.selected = this.id;
+
+        // remove all classes before choice
+        listItems.forEach((listItem) => {
+          listItem.classList.remove('correctAnswer', 'wrongAnswer');
+        });
+
+        // found current list item
+        const currentLi = document.getElementById(`${this.id}`);
+
+        //found correct list item and add css class correct
+        const correctLi = document.querySelector(`#${currentQuestion.correct}`);
+        correctLi.classList.add('correctAnswer');
+
+        // logics for add class wrongAnswer
+        if (currentLi.id === currentQuestion.correct) {
+          currentLi.classList.add('correctAnswer');
+        } else {
+          currentLi.classList.add('wrongAnswer');
         }
-      });
+      }
+    });
   }
   //  next question button will move to next question if the questions are finished it will lunch the final page
   if (quizData.currentQuestionIndex === quizData.questions.length - 1) {
