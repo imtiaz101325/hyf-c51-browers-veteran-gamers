14ff9598ad09f1ad159bcdab8d7bee25530d7fa5
khiro963 <khiro9633@gmail.com>
2025-01-18 06:34:20 +0100

    adjusted the style and added background img

--- src/views/finalPageView.js
+++ src/views/finalPageView.js
@@ -5,8 +5,8 @@ export const createFinalPAgeElement = () => {
   const finalPageElement = document.createElement('div');
   const playerName = localStorage.getItem('playerName') || 'player';
   finalPageElement.innerHTML = String.raw`
-    <p> Well played! ${playerName} </p>
-    <p> Your score result is : ${quizData.scoreResult} out of ${quizData.questions.length} </p>
-    <button id = ${RESTART_GAME_BUTTON_ID}>Restart game</button>`;
+    <p class="final-message"> Well done! ${playerName} </p>
+    <p class="final-message"> Your score result is : ${quizData.scoreResult} out of ${quizData.questions.length} </p>
+    <button id = ${RESTART_GAME_BUTTON_ID}>Restart the quiz</button>`;
   return finalPageElement;
 };
--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -7,8 +7,8 @@ import { PLAYER_NAME_INPUT_ID } from '../constants.js';
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-    <h1>Welcome</h1>
-    <label for="${PLAYER_NAME_INPUT_ID}"> Player name </label>
+    <h1 class = "welcome-text">Welcome to the veteran gamers test</h1>
+    <label for="${PLAYER_NAME_INPUT_ID}" class="player-name"> Player name </label>
     <input type="text" id ="${PLAYER_NAME_INPUT_ID}">
     <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
   `;
